ecb6eca24dfad00517ef1fc6e03f1559be484a61
mingyu.k <mingyu.k@navercorp.com>
2024-08-16 00:46:32 +0900

    feat: change path

--- app/travel-at/page.tsx
+++ app/travel-at/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
                             <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">🇯🇵</div>
                             <h2 className="h4 fw-bolder">일본</h2>
                             <p>일본의 돈키호테 등에서 꼭 사야 할 인기 상품을 소개합니다. 쇼핑 리스트에 빠질 수 없는 현지 상품을 한눈에 확인하세요!</p>
-                            <a className="text-decoration-none" href="/travel-at/japan/donki">
+                            <a className="text-decoration-none" href="/japan/donki">
                                 추천 상품 보기
                                 <i className="bi bi-arrow-right"></i>
                             </a>
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -5,7 +5,7 @@ export default function Navigation() {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container px-4 px-lg-5">
-                    <a className="navbar-brand" href="/travel-at">Travel At</a>
+                    <a className="navbar-brand" href="/">Travel At</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation"><span
@@ -13,12 +13,12 @@ export default function Navigation() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                             <li className="nav-item"><a className="nav-link active" aria-current="page"
-                                                        href="/travel-at">Home</a></li>
+                                                        href="/">Home</a></li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
                                    data-bs-toggle="dropdown" aria-expanded="false">일본</a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="/travel-at/japan/donki">돈키호테</a></li>
+                                    <li><a className="dropdown-item" href="/japan/donki">돈키호테</a></li>
                                 </ul>
                             </li>
                         </ul>
