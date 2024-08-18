821f32864f18e3ede8da42e00ac4f9a0ce858744
mingyu.k <mingyu.k@navercorp.com>
2024-08-18 15:11:22 +0900

    feat: Install vercel speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 import './globals.css'
@@ -36,6 +37,7 @@ export default function RootLayout({
         <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
         </body>
         </html>
     );
